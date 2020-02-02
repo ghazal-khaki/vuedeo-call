b7791aac1b38c6155f99c1eaef03732bc002151c
ghazal-khaki <ghazalkhaki3@gmail.com>
2020-02-02 17:04:23 +0330

    find some bugs

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-    <VuedeoCall />
+    <HelloWorld />
   </div>
 </template>
 
 <script>
-import VuedeoCall from './components/VuedeoCall'
+import HelloWorld from './components/HelloWorld'
 
 export default {
   name: 'app',
   components: {
-    VuedeoCall
+    HelloWorld
   }
 }
 </script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,13 +1,118 @@
 <template>
   <div class="hello">
+    <video
+      id="caller"
+      autoplay
+      playsinline
+    ></video>
+    <video
+      id="callee"
+      autoplay
+      playsinline
+    ></video>
+    <button @click="call"> CALL </button>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
+  data: function () {
+    return {
+      localStream: null,
+      remoteStream: null,
+      servers: null,
+      constraints: { video: true, audio: true },
+      localPeerConnection: null,
+      remotePeerConnection: null
+    }
+  },
   props: {
     msg: String
+  },
+  methods: {
+    call () {
+      this.localPeerConnection = new RTCPeerConnection(this.servers)
+      console.log('local peer connection has been created')
+      this.localPeerConnection.addEventListener('icecandidate', this.handleConnection)
+      this.localPeerConnection.addEventListener(
+        'iceconnectionstatechange',
+        this.handleConnectionChange)
+
+      this.remotePeerConnection = new RTCPeerConnection(this.servers)
+      console.log('remote peer connection has been created')
+      this.remotePeerConnection.addEventListener('icecandidate', this.handleConnection)
+      this.remotePeerConnection.addEventListener(
+        'iceconnectionstatechange',
+        this.handleConnectionChange)
+
+      this.remotePeerConnection.addEventListener('addstream', this.gotRemoteMediaStream)
+
+      this.localPeerConnection.addStream(this.localStream)
+      console.log('added local stream to localPeerConnection')
+
+      console.log('localPeerConnection create offer start')
+      this.localPeerConnection.createOffer({
+        offerToReceiveVideo: 1
+      }).then(this.createdOffer).catch()
+    },
+    handleConnection (event) {
+      console.log(event)
+      const peerConnection = event.target
+      const iceCandidate = event.candidate
+      if (iceCandidate) {
+        const newIceCandidate = new RTCIceCandidate(iceCandidate)
+        const otherPeer = this.getOtherPeer(peerConnection)
+        console.log(otherPeer)
+        debugger
+        otherPeer.addIceCandidate(newIceCandidate)
+          .then(() => {
+            console.log('addIceCandidate success')
+          })
+          .catch((err) => {
+            console.log(err)
+            console.log('addIceCandidate does not success')
+          })
+      }
+    },
+    handleConnectionChange (event) {
+      const peerConnection = event.target
+      console.log('ICE state change event: ', peerConnection.iceConnectionState)
+    },
+    gotRemoteMediaStream (event) {
+      const mediaStream = event.localStream
+      this.remoteStream = mediaStream
+    },
+    createdOffer (description) {
+      this.localPeerConnection.setLocalDescription(description)
+        .then()
+        .catch()
+
+      this.remotePeerConnection.setRemoteDescription(description)
+        .then()
+        .catch()
+
+      this.remotePeerConnection.createAnswer()
+        .then(this.createdAnswer)
+        .catch()
+    },
+    createdAnswer (description) {
+      this.remotePeerConnection.setLocalDescription(description)
+        .then()
+        .catch()
+    },
+    getOtherPeer (peerConnection) {
+      return (peerConnection === this.localPeerConnection) ? this.localPeerConnection : this.remotePeerConnection
+    }
+  },
+  mounted () {
+    navigator.mediaDevices.getUserMedia(this.constraints)
+      .then(resp => {
+        document.querySelector('#caller').srcObject = resp
+        this.localStream = resp
+        console.log('Received local stream')
+      }).catch(err => console.log(err))
+    this.remoteStream = document.querySelector('#callee').srcObject
   }
 }
 </script>
